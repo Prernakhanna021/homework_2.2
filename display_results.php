bb874ac04a9c42dcdb1c22734ff6aec7418b1983
Prerna Khanna <pk462@njit.edu>
2017-02-12 19:18:47 -0500

    change od date

--- display_results.php
+++ display_results.php
@@ -69,6 +69,7 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
+        <p>This calculation was done on <?php echo date('2/1/2017'); ?>.</p>
     </main>
 </body>
 </html>
